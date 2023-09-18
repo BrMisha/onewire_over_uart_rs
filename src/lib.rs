d0b622859de149bc03757c31e09bc385d5c5eae2
Misha <br.misha@gmail.com>
2023-09-18 12:56:03 +0300

    particly impl search rom

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(std), no_std)]
 
-mod low_level;
+pub mod low_level;
 
 pub enum Baudrate {
     Br9600,
@@ -28,8 +28,36 @@ pub enum Error {
 }
 
 const OW_ROMCODE_SIZE: usize = 8;
-#[derive(Debug, Copy, Clone, Default)]
+
+#[derive(Debug, Copy, Clone, Default, PartialOrd, PartialEq)]
 pub struct Rom(pub [u8; OW_ROMCODE_SIZE]);
+impl Rom {
+    pub fn family_code(&self) -> u8 {
+        self.0[0]
+    }
+}
+
+impl core::str::FromStr for Rom {
+    type Err = core::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 23 {
+            let _ = u8::from_str_radix("", 16)?; // this causes a ParseIntError::Empty
+        }
+        Ok(Rom {
+            0: [
+                u8::from_str_radix(&s[0..2], 16)?,
+                u8::from_str_radix(&s[3..5], 16)?,
+                u8::from_str_radix(&s[6..8], 16)?,
+                u8::from_str_radix(&s[9..11], 16)?,
+                u8::from_str_radix(&s[12..14], 16)?,
+                u8::from_str_radix(&s[15..17], 16)?,
+                u8::from_str_radix(&s[18..20], 16)?,
+                u8::from_str_radix(&s[21..23], 16)?,
+            ],
+        })
+    }
+}
 
 pub enum Cmd {
     SEARCHROM = 0xF0,
@@ -37,16 +65,6 @@ pub enum Cmd {
     MATCHROM = 0x55,
     SKIPROM = 0xCC,
 }
-
-/*
-#define	OW_SEARCH_FIRST	0xFF		// start new search
-#define	OW_PRESENCE_ERR	0xFF
-#define	OW_DATA_ERR	    0xFE
-#define OW_LAST_DEVICE	0x00		// last device found
-//			0x01 ... 0x40: continue searching
-*/
-
-
 
 pub enum FamilyCode {
     DS1990 = 1,
@@ -73,16 +91,45 @@ pub fn read_rom(uart: &mut dyn UartTrait) -> Result<Rom, Error> {
 
     let mut rom: Rom = Default::default();
 
-    for i in 0..8 {
-        rom.0[i] = low_level::ow_read_byte(uart);
+    for i in rom.0.iter_mut() {
+        *i = low_level::ow_read_byte(uart);
     }
 
     Ok(rom)
 }
 
-#[cfg(test)]
-mod tests {
+pub fn match_rom(uart: &mut dyn UartTrait, rom: &Rom) -> Result<(), Error> {
+    if reset(uart) != true {
+        return Err(Error::ResetError);
+    }
 
-    #[test]
-    fn it_works() {}
+    low_level::ow_write_byte(uart, Cmd::MATCHROM as u8);
+
+    let mut rom: Rom = Default::default();
+
+    for x in rom.0 {
+        low_level::ow_write_byte(uart, x);
+    }
+
+    Ok(())
 }
+
+/*
+pub struct DeviceSearchIter<'a> {
+    search: Option<DeviceSearch>,
+    wire: &'a mut dyn UartTrait,
+}
+
+impl<'a> Iterator for DeviceSearchIter<'a> {
+    type Item = Result<Rom, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut search = self.search.take()?;
+        let result = self
+            .wire
+            .search_next(&mut search)
+            .transpose()?;
+        self.search = Some(search);
+        Some(result)
+    }
+}*/
